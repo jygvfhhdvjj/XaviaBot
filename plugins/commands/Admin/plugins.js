10b9546ca9b842251c4971891abb30b7eea2c9a4
jygvfhhdvjj <157843631+jygvfhhdvjj@users.noreply.github.com>
2024-01-28 19:30:54 +0300

    Update plugins.js
    Gg

--- plugins/commands/Admin/plugins.js
+++ plugins/commands/Admin/plugins.js
@@ -1,4 +1,5 @@
-const config = {
+
+/*const config = {
     name: "plugins",
     aliases: ["pl", "plg", "plugin"],
     description: "Manage plugins",
@@ -34,44 +35,4 @@ async function onCall({ message, args, getLang }) {
         if (query === "reload") {
             delete global.plugins;
             global.plugins = new Object({
-                commands: new Map(),
-                commandsAliases: new Map(),
-                commandsConfig: new Map(),
-                customs: new Number(0),
-                events: new Map(),
-                onMessage: new Map()
-            });
 
-            for (const lang in global.data.langPlugin) {
-                for (const plugin in global.data.langPlugin[lang]) {
-                    if (plugin == config.name) continue;
-                    delete global.data.langPlugin[lang][plugin];
-                }
-            }
-
-            delete global.data.temps;
-            global.data.temps = new Array();
-
-            await global.modules.get("loader").loadPlugins();
-            return message.reply(getLang("result.reload"));
-        } else if (query == 'list') {
-            return message.reply(getLang("result.list", {
-                commands: global.plugins.commands.size,
-                events: global.plugins.events.size,
-                onMessage: global.plugins.onMessage.size,
-                customs: global.plugins.customs
-            }));
-        } else {
-            message.reply(getLang("invalid.query"));
-        }
-    } catch (e) {
-        console.error(e);
-        message.reply(getLang("error.unknow"));
-    }
-}
-
-export default {
-    config,
-    langData,
-    onCall
-}
